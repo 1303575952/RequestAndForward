9a7ac38ceb5bbbc4020d3ef4401d3b127921d5e3
1303575952@qq.com <1303575952@qq.com>
2018-12-31 14:28:16 +0800

    modify project

--- src/test/java/com/sxu/forward/ForwardApplicationTests.java
+++ src/test/java/com/sxu/forward/ForwardApplicationTests.java
@@ -2,11 +2,7 @@ package com.sxu.forward;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ForwardApplicationTests {
 
 	@Test
